f04f43303d492e502bb482850ff396fa167764d1
rrose98 <36145326+rrose98@users.noreply.github.com>
2018-02-06 08:18:26 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+<head><title>Web Dev</title></head>
+<style>
+div{
+background-color:pink;
+}
+</style>
+<div>
+<h1><b>Personal Data</b></h1>
+</div>
+<body>
+<p><h5><b>Name:</b> Remeticado, Ria Rose A.</h5></p>
+<p><h5><b>Address:</b> Cabiangon, Pinamungajan Cebu</h5></p>
+<p><h5><b>Age:</b> 19</h5></p>
+<p><h5><b>Date of Birth:</b> June 22,1998</h5></p>
+<p><h5><b>Place of Birth:</b> Pit-os, Lantawan, San Fernando Cebu</h5></p>
+<p><h5><b>Gender:</b> Female</h5></p>
+<p><h5><b>Religion:</b> Roman Catholic</h5></p>
+<p><h5><b>Nationality:</b> Filipino</h5></p>
+<p><h5><b>Hobbies:</b> Singing and Dancing, Reading and Writing</h5></p>
+<div>
+<h1><b>Educational Attainment</b></h1>
+</div>
+<p><h5><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbspName of School:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;;A.Y.:&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Honors Received:</b></h5></p>
+<p><h5>Pit-os Elementary School&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2010&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4th Honorable Mention</h5></p>
+<p><h5>Pit-os National High School&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;2014&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1st Honorable Mention</h5></p>
+<p><h5>Cebu Technological University</h5></p>
+<div>
+<h1><b>Biography</b></h1>
+</div>
+<p><h5>&nbsp;&nbsp;&nbsp;&nbsp;My name is Remeticado, Ria Rose A. currently living in Cabiangon, Pinamungajan Cebu with my live-in partner Sasuta, Jufiel C.</h5></p> 
+<p><h5>Taking up Bachelor of Science in Information and Communication Technology(BSICT) at Cebu Technological University(CTU) Naga-Extension Campus.</h5></p>
+<p><h5>I ride the bus everyday from home to school and the other way.</h5></p>
+<p><h5>&nbsp;&nbsp;&nbsp;&nbsp;I had a daughter at an early age of 17. Luckily my family took it easily and understand the situation.</h5></p>
+<p><h5>My mother decided to let me live with my boyfriend for she has nothing she can do to make things right. So Jufiel's family let us live in their old house.</h5></p>
+<p><h5>The house isn't big nor small, just enough for a small family which is normally composed of a maximum of 3 children. During the first month of stay, everything seemed normal.</h5></p>
+<p><h5>It felt like we really are bound but then things seemed to go wrong, misunderstandings and problems started building such wall that love alone cannot conquer./h5>/p>
+<p><h5>Trust had become the downfall of our relationship. So I decided to ask my parents to raise my child while I pursue my studies. My mother agreed to it, but when classes is fast approaching,</h5></p>
+<p><h5>Jufiel decided to show. At first, I didn't plan to reconcile but when I saw how my daughter longed for her father. It took all my courage to push him away from us./h5></p>
+<p><h5>Classes started, instead of continuing the course i had taken which is Bachelor of Technical Teaching Education (BTTE). I decided to take another course, Bachelor of Science in Information and Communication Technology.</h5></p>
+</body>
+</html>
